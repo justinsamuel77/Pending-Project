15a86dd6677845845b7a83329a3cd4a2d5b9fb19
Justin Samuel S <120775182+justinsamuel77@users.noreply.github.com>
2023-01-31 19:23:12 +0530

    Update Todos.js

--- React Tutorial/frontend/src/components/Todos.js
+++ React Tutorial/frontend/src/components/Todos.js
@@ -1,24 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { removeTodo, findTodo, addTodo } from "../Redux/todoapp/actions/index.js";
+import { Link, Button, Form, Container } from "react-bootstrap";
+import {
+  removeTodo,
+  addTodo,
+  handleEditSubmit,
+} from "../Redux/todoapp/actions/index.js";
 
 export const Todos = () => {
   const todos = useSelector((state) => state.operationsReducers);
   console.log(todos, "todos");
   const [check, setCheck] = useState(false);
-//   const dispatch = useDispatch(false);
-  const [inputValue, setInputValue] = useState("");
-  const [editTodo, setEditTodo] = useState(false);
-  const [todoValue, setTodoValue] = useState("")
+  const [editTodo, setEditTodo] = useState("");
+  const [todoValue, setTodoValue] = useState("");
   const dispatch = useDispatch();
-  const [editFormVisibility, setEditFormVisibility] = useState(false)
+  const [editValue, setEditValue] = useState("");
+  const [editFormVisibility, setEditFormVisibility] = useState(false);
 
-  const handleEditClick = () => {
+  useEffect(() => {
+    setEditValue(editTodo.todo);
+  }, [editTodo]);
+
+  const handleEditClick = (getTodo) => {
     setEditFormVisibility(true);
-  }
-
-  let date = new Date();
-  let time = date.getTime();
+    setEditTodo(getTodo);
+    console.log(getTodo, "get");
+  };
+  // console.log(editTodo, "edditTodo");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -34,12 +42,6 @@ export const Todos = () => {
     dispatch(addTodo(todoObj));
   };
 
-  // const handleEditClick = async (editTodo) => {
-  //   console.log(editTodo.todo, "hec");
-  //   setEditTodo(true);
-  //   setInputValue(editTodo.todo);
-  // };
-
   const handleCheck = (e) => {
     if (e.target.checked) {
       setCheck(true);
@@ -48,91 +50,125 @@ export const Todos = () => {
     }
   };
 
+  const editSubmit = (e) => {
+    e.preventDefault();
+    let editedObj = {
+      id: editTodo.id,
+      todo: editValue,
+      completed: false,
+    };
+    dispatch(handleEditSubmit(editedObj));
+    console.log(editedObj, "editobj");
+    setEditValue("");
+  };
+
+  console.log(editTodo, "ET");
   return (
     <>
-    {editFormVisibility===false?(
-      <div>
-      <input
-        type="text"
-        name="name"
-        id="name"
-        value={todoValue}
-        onChange={(e) => setTodoValue(e.target.value)}
-      />
-      <button type="button" onClick={handleSubmit}>
-        Save
-      </button>
-    </div>
-    ):(
-      <div>
-        <label>Update todo</label>
-      <input
-        type="text"
-        name="name"
-        id="name"
-      />
-      <button type="button">
-        Update
-      </button>
-    </div>
-    )}
-    
-      <h1>Todos</h1>
-      {todos.map((getTodo) => {
-        return (
-          <>
-            <div>
-              {check == false ? (
+      <Container fluid>
+        {editFormVisibility === false ? (
+          <div class="col-md-8 offset-md-2">
+            <Form.Control
+              className="inputcontrol"
+              size="sm"
+              type="text"
+              name="name"
+              id="name"
+              placeholder="type something..."
+              value={todoValue}
+              onChange={(e) => setTodoValue(e.target.value)}
+            />
+            <Button
+              type="button"
+              style={{ marginTop: "15px" }}
+              onClick={handleSubmit}
+            >
+              Save
+            </Button>
+          </div>
+        ) : (
+          <div>
+            <form class="col-md-8 offset-md-2">
+              <label>Update todo</label>
+              <Form.Control
+                type="text"
+                name="name"
+                id="name"
+                value={editValue}
+                onChange={(e) => setEditValue(e.target.value)}
+              />
+              <Button
+                style={{ marginTop: "15px" }}
+                type="button"
+                onClick={editSubmit}
+              >
+                Update
+              </Button>
+            </form>
+          </div>
+        )}
+
+        <div className="checktodo">
+          <h1>Welcome</h1>
+          {todos.map((getTodo) => {
+            return (
+              <>
                 <div>
-                  {editFormVisibility===false&&(
-                    <input
-                    type="checkbox"
-                    name="name"
-                    id="name"
-                    value={getTodo.id}
-                    onClick={handleCheck}
-                  />
+                  {check == false ? (
+                    <div>
+                      <p>Inprogress</p>
+                      {editFormVisibility === false && (
+                        <input
+                          type="checkbox"
+                          name="name"
+                          id="name"
+                          value={getTodo.id}
+                          onClick={handleCheck}
+                        />
+                      )}
+                      {console.log(getTodo, "gettodo")}
+                      <div className="btntodo">
+                        <p>{getTodo.todo}</p>
+                        {editFormVisibility === false}
+                        <Button
+                          type="button"
+                          onClick={() => handleEditClick(getTodo)}
+                        >
+                          Edit
+                        </Button>
+                        &nbsp;
+                        <Button
+                          type="button"
+                          onClick={() => dispatch(removeTodo(getTodo.id))}
+                        >
+                          Delete
+                        </Button>
+                      </div>
+                    </div>
+                  ) : (
+                    <div style={{ marginLeft: "900px" }}>
+                      <p>Completed</p>
+                      <input
+                        type="checkbox"
+                        name="name"
+                        id="name"
+                        value={getTodo.id}
+                        onClick={handleCheck}
+                      />
+                      <p>{getTodo.todo}</p>
+                      <Button type="button" onClick={handleEditClick}>
+                        Edit
+                      </Button>
+                      &nbsp;
+                      <Button type="button">Delete</Button>
+                    </div>
                   )}
-                  <div>
-                  <p>{getTodo.todo}</p>
-                  {editFormVisibility===false}
-                  <button
-                    type="button"
-                    onClick={() => handleEditClick(getTodo)}
-                  >
-                    Edit
-                  </button>
-                  &nbsp;
-                  <button
-                    type="button"
-                    onClick={() => dispatch(removeTodo(getTodo.id))}
-                  >
-                    Delete
-                  </button>
-                  </div>
                 </div>
-              ) : (
-                <div style={{ marginLeft: "400px" }}>
-                  <input
-                    type="checkbox"
-                    name="name"
-                    id="name"
-                    value={getTodo.id}
-                    onClick={handleCheck}
-                  />
-                  <p>{getTodo.todo}</p>
-                  <button type="button" onClick={handleEditClick}>
-                    Edit
-                  </button>
-                  &nbsp;
-                  <button type="button">Delete</button>
-                </div>
-              )}
-            </div>
-            
-          </>
-        );
-      })}
+              </>
+            );
+          })}
+        </div>
+      </Container>
     </>
   );
 };
